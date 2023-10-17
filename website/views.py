d68f68865e89b22e07f572b767b84292a430721c
thesheox <thesheox@gmail.com>
2023-10-18 00:34:00 +0330

    static + template site on local

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
-    return render(request,"websites/home.html")
+    return render(request,"websites/index.html")
 
 def about_view(request):
     return render(request,"websites/about.html")

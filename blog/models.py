53ff32c291c7332b71a125fe739ae94ec592203d
thesheox <thesheox@gmail.com>
2023-11-10 22:52:02 +0330

    add database to admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    #image
+    title = models.CharField(max_length=255)
+    content=models.TextField()
+    #tag
+    #category
+    counted_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    published_date=models.DateTimeField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return " {} - {}".format(self.title,self.id-1)
+    
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path("",include("website.urls")),
     path("blog",include("blog.urls"))
 ]

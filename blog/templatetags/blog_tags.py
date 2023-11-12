a7f98a50ebf15cb8b40884d4956f0c10cf44aa4e
thesheox <thesheox@gmail.com>
2023-11-12 23:52:05 +0330

    revert order by published date

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -22,5 +22,5 @@ def popularposts():
 
 @register.inclusion_tag('blog/blog-popular-posts.html')
 def latestposts(arg=3):
-    posts=Post.objects.filter(status=1).order_by('published_date')[:arg]
+    posts=Post.objects.filter(status=1).order_by('-published_date')[:arg]
     return {'posts':posts}

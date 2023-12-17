b7c222b500ba5aa7b60d32529e040a4cb384dea2
thesheox <thesheox@gmail.com>
2023-12-17 11:46:57 +0330

    login form added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 def login_view(request):
-    
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password'] 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        
     return render(request, 'accounts/login.html')
 
 
